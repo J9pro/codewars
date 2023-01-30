a7967cad7be1f24b12db3ec82002c78fad49c455
J9pro <janine239@gmail.com>
2023-01-30 20:28:15 +0100

    solution

--- 6kyu/n-th-Fibo.js
+++ 6kyu/n-th-Fibo.js
@@ -6,3 +6,12 @@
 // Because 2 is the 4th number in the Fibonacci Sequence.
 
 // For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+
+function nthFibo(n) {
+    // Return the n-th number in the Fibonacci Sequence
+    let arr = [0, 1];
+    for (let i = 1; i < n; i++) {
+        arr.push(arr[i - 1] + arr[i]);
+    }
+    return n > 0 ? arr[n - 1] : 0;
+}
