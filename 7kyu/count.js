657af39bf42eb38e369ffae53fc8dc98242e4cf2
J9pro <janine239@gmail.com>
2023-02-04 12:19:47 +0100

    another solution

--- 7kyu/count.js
+++ 7kyu/count.js
@@ -14,3 +14,13 @@ function getDivisorsCnt(n) {
     }
     return count;
 }
+
+//Other solution
+
+function getDivisorsCnt(n) {
+    for (var d = 0, i = n; i > 0; i--) {
+        if (n % i == 0) d++;
+    }
+
+    return d;
+}
