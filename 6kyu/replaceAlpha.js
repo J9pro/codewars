2ed446a5bafa5308d902f5a657f8b6cdf6f701a2
J9pro <janine239@gmail.com>
2023-02-22 23:04:50 +0100

    edit

--- 6kyu/replaceAlpha.js
+++ 6kyu/replaceAlpha.js
@@ -9,7 +9,7 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"(as a string)
 
 
-
+//solution
 function alphabetPosition(text) {
     let letters = "abcdefghijklmnopqrstuvwxyz";
     let position = [];
